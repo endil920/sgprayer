d96f5edd09bc83adcd09fa10bf012d56c7a9da0b
Eng <engd@sea-engd-m2.ds.ad.adp.com>
2016-01-01 16:58:42 -0800

    add step 1 of backend integration for POST

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-
+var bodyParser = require('body-parser');
 app.use(express.static('public'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.engine('.html', require('jade').renderFile);
 
@@ -14,8 +15,12 @@ app.get('/views/summary', function(req, res) {
 	res.render(__dirname + '/public/views/summary.html');
 });
 app.get('/:group', function(req, res) {
-	console.log(req.params.group);
 	res.sendFile(__dirname + '/public/index.html');
+});
+app.post('/request/:group', function(req, res) {
+//TODO: add to group collection
+//TODO: trigger socket event for all others in the group
+	console.log(req.body);
 });
 
 
