c53baa8a81c85e8c2b55586af39620813206d895
Eng <engd@sea-engd-m2.ds.ad.adp.com>
2016-01-01 16:31:22 -0800

    add views and routing

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/public/js/index.html');
+app.use(express.static('public'));
+
+
+app.engine('.html', require('jade').renderFile);
+
+app.get('/views/update', function(req, res) {
+	res.sendFile(__dirname + '/public/views/update.html');
 });
+app.get('/views/summary', function(req, res) {
+	res.render(__dirname + '/public/views/summary.html');
+});
+app.get('/:group', function(req, res) {
+	console.log(req.params.group);
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+
 app.listen(3000);
 console.log("listening on port 3000");
