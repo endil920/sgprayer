ccd85be23d5181d0876f0da880c577e42cb4cae6
Dillon Eng <dillon@MacBook-Pro.local>
2016-01-24 23:24:00 -0800

    remove WeeklyRequestsSchema as we now use the FullRequest

--- models/weeklyRequests.js
+++ models/weeklyRequests.js
@@ -3,27 +3,12 @@ var findOrCreate = require('mongoose-findorcreate');
 
 var Schema = mongoose.Schema;
 
-var requestSchema = new Schema({
-	name: String,
-    date: Date,
-    message: String});
-
 var groupSchema = new Schema({
   meetingDay: Number,
     name: String
 });
-
 groupSchema.plugin(findOrCreate);
 var Group = mongoose.model('Group', groupSchema);
-
-var weeklyRequestsSchema = new Schema({
-	weekNumber: Number,
-    group: {type: mongoose.Schema.Types.ObjectId, ref: 'Group'}, 
-    requests: [requestSchema]
-});
-
-weeklyRequestsSchema.plugin(findOrCreate);
-var WeeklyRequests = mongoose.model('WeeklyRequests', weeklyRequestsSchema);
 
 var fullRequestSchema = new Schema({
     name: String,
@@ -34,6 +19,5 @@ var fullRequestSchema = new Schema({
 });
 var FullRequest = mongoose.model('FullRequest', fullRequestSchema);
 
-module.exports.WeeklyRequests = WeeklyRequests;
 module.exports.Group = Group;
 module.exports.FullRequest = FullRequest;
